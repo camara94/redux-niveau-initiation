94434eb265da417dd578e43adaa6f3f945b28f98
Laby Damaro Camara <ldamaro98@gmail.com>
2021-08-12 03:25:49 +0200

    création de projet demo-react-toolkit

---
+++ demo-redux-toolkit/src/App.js
@@ -0,0 +1,25 @@
+import logo_redux from './logo-redux1.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo_redux} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://redux-toolkit.js.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Redux Toolkit
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
